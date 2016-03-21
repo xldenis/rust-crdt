b755c140a96511f0c5a20707645b17e2d5a23506
Tyler Neely <t@jujit.su>
2016-03-21 01:36:15 -0700

    flesh out ORSWOT a bit more

--- src/orswot.rs
+++ src/orswot.rs
@@ -8,13 +8,13 @@ use std::collections::BTreeMap;
 use super::VClock;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd, RustcEncodable, RustcDecodable)]
-pub struct Orswot<Member: Ord, Actor: Ord + Clone> {
+pub struct Orswot<Member: Ord + Clone, Actor: Ord + Clone> {
     clock: VClock<Actor>,
     entries: BTreeMap<Member, VClock<Actor>>,
     deferred: BTreeMap<VClock<Actor>, Vec<Member>>,
 }
 
-impl<Member: Ord, Actor: Ord + Clone> Orswot<Member, Actor> {
+impl<Member: Ord + Clone, Actor: Ord + Clone> Orswot<Member, Actor> {
     pub fn new() -> Orswot<Member, Actor> {
         Orswot {
             clock: VClock::new(),
@@ -23,15 +23,38 @@ impl<Member: Ord, Actor: Ord + Clone> Orswot<Member, Actor> {
         }
     }
 
-    pub fn add() {}
-    pub fn add_all() {}
+    pub fn add(&mut self, member: Member, actor: Actor) {
+        let counter = self.clock.increment(actor.clone());
+
+        // TODO(tyler) riak_dt doesn't merge entry clocks on add.  Is this a bug?
+        let mut entry_clock = VClock::new();
+        entry_clock.witness(actor, counter).unwrap();
+
+        self.entries.insert(member, entry_clock);
+    }
+
+    pub fn add_all(&mut self, members: Vec<(Member, Actor)>) {
+        members.into_iter().map(|(member, actor)| self.add(member, actor));
+    }
 
     // drop the element out, ignoring the vclock for the element and orswot
-    pub fn remove() {}
-    pub fn remove_all() {}
-    pub fn update() {}
-    pub fn value() {}
-    pub fn merge() {}
+    pub fn remove(&mut self, member: Member) -> Option<VClock<Actor>> {
+        self.entries.remove(&member)
+    }
+
+    pub fn remove_all(&mut self, members: Vec<Member>) -> Vec<Option<VClock<Actor>>> {
+        members.into_iter().map(|member| self.remove(member)).collect()
+    }
+
+    pub fn value(&self) -> Vec<Member> {
+        self.entries.keys().cloned().collect()
+    }
+
+    pub fn merge(&mut self, other: Orswot<Member, Actor>) {
+        self.clock.merge(other.clock);
+    }
+
+    fn merge_deferred(&mut self, deferred: BTreeMap<VClock<Actor>, Vec<Member>>) {}
     pub fn equal() {}
     pub fn precondition_context() {}
     pub fn stats() {}
--- src/vclock.rs
+++ src/vclock.rs
@@ -55,9 +55,12 @@ impl<A: Ord + Clone> VClock<A> {
     /// assert!(a > b);
     /// ```
     ///
-    pub fn witness(&mut self, actor: A, counter: Counter) {
+    pub fn witness(&mut self, actor: A, counter: Counter) -> Result<(), ()> {
         if !self.contains_descendent_element(&actor, &counter) {
             self.dots.insert(actor, counter);
+            Ok(())
+        } else {
+            Err(())
         }
     }
 
@@ -101,7 +104,7 @@ impl<A: Ord + Clone> VClock<A> {
     ///
     pub fn merge(&mut self, other: VClock<A>) {
         for (w, c) in other.dots.into_iter() {
-            self.witness(w, c)
+            self.witness(w, c);
         }
     }
 
@@ -137,11 +140,12 @@ impl<A: Ord + Clone> VClock<A> {
 
     /// Return the difference in dots between self and a provided dots map.
     pub fn subtract_dots(&self, dots: BTreeMap<A, Counter>) -> BTreeMap<A, Counter> {
-        let mut ret = self.dots.clone();
-        for (actor, counter) in dots.iter() {
-            let diff = ret.get(actor).map(|c| *c).unwrap_or(0) - counter;
-            if diff <= 0 {
-                ret.remove(actor);
+        let mut ret = BTreeMap::new();
+        for (actor, counter) in self.dots.iter() {
+            let other = dots.get(actor).map(|c| *c).unwrap_or(0);
+            let diff = counter - other;
+            if diff > 0 {
+                ret.insert(actor.clone(), diff);
             }
         }
         ret
@@ -157,10 +161,10 @@ mod tests {
         assert!(VClock::<i8>::new() == VClock::new());
 
         let (mut a, mut b) = (VClock::new(), VClock::new());
-        a.witness("A", 1);
-        a.witness("A", 2);
-        a.witness("A", 0);
-        b.witness("A", 1);
+        a.witness("A", 1).unwrap();
+        a.witness("A", 2).unwrap();
+        assert!(a.witness("A", 0).is_err());
+        b.witness("A", 1).unwrap();
 
         // a {A:2}
         // b {A:1}
@@ -169,7 +173,7 @@ mod tests {
         assert!(b < a);
         assert!(a != b);
 
-        b.witness("A", 3);
+        b.witness("A", 3).unwrap();
         // a {A:2}
         // b {A:3}
         // expect: b dominates
@@ -177,7 +181,7 @@ mod tests {
         assert!(a < b);
         assert!(a != b);
 
-        a.witness("B", 1);
+        a.witness("B", 1).unwrap();
         // a {A:2, B:1}
         // b {A:3}
         // expect: concurrent
@@ -185,7 +189,7 @@ mod tests {
         assert!(!(a > b));
         assert!(!(b > a));
 
-        a.witness("A", 3);
+        a.witness("A", 3).unwrap();
         // a {A:3, B:1}
         // b {A:3}
         // expect: a dominates
@@ -193,7 +197,7 @@ mod tests {
         assert!(b < a);
         assert!(a != b);
 
-        b.witness("B", 2);
+        b.witness("B", 2).unwrap();
         // a {A:3, B:1}
         // b {A:3, B:2}
         // expect: b dominates
@@ -201,7 +205,7 @@ mod tests {
         assert!(a < b);
         assert!(a != b);
 
-        a.witness("B", 2);
+        a.witness("B", 2).unwrap();
         // a {A:3, B:2}
         // b {A:3, B:2}
         // expect: equal
