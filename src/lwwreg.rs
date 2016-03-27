3573cb8994062ac5e868476bd3a10bc2884e4de6
Tyler Neely <t@jujit.su>
2016-03-27 11:27:17 -0700

    encodable lwwreg

--- src/lwwreg.rs
+++ src/lwwreg.rs
@@ -1,10 +1,22 @@
+use bincode::SizeLimit;
+use bincode::rustc_serialize::{encode, decode, DecodingResult};
+use rustc_serialize::{Encodable, Decodable};
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash, RustcEncodable, RustcDecodable)]
-pub struct LWWReg<T, A: Ord> {
+pub struct LWWReg<T: Encodable + Decodable, A: Ord + Encodable + Decodable> {
     pub value: T,
     pub dot: A,
 }
 
-impl<T, A: Ord> LWWReg<T, A> {
+impl<T: Encodable + Decodable, A: Ord + Encodable + Decodable> LWWReg<T, A> {
+    pub fn to_binary(&self) -> Vec<u8> {
+        encode(&self, SizeLimit::Infinite).unwrap()
+    }
+
+    pub fn from_binary(encoded: Vec<u8>) -> DecodingResult<Self> {
+        decode(&encoded[..])
+    }
+
     pub fn merge(&mut self, other: LWWReg<T, A>) {
         if other.dot > self.dot {
             self.value = other.value;
