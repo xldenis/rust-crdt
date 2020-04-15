aac387f5bdfb06424e47706ee1c04eee4b436b12
Thomas Heck <t@b128.net>
2020-04-15 15:08:44 +0200

    (nix): add missing rust packages

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
-with import <nixpkgs> {};
 
-stdenv.mkDerivation {
-    name = "crdts";
-    buildInputs = with pkgs; [
-        cargo
-        git
-    ];
-}
+let
+    pkgs = import <nixpkgs> {};
+in 
+    pkgs.stdenv.mkDerivation {
+        name = "crdts";
+        buildInputs = [
+            pkgs.cargo
+            pkgs.rustc
+            pkgs.rustfmt
+            pkgs.rls
+        ];
+    }
